c9449fc1560b30a70011fabccbb7bd6333d3d2a5
Adrià Cabrera <adriacabrera2007@gmail.com>
2024-05-29 17:22:44 +0200

    feat: Add profile SVG icon component
    The code changes include the addition of a new SVG icon component called ProfileSVG. This component renders an SVG icon for a user profile. It is used in multiple places throughout the application to display user profile images. The purpose of this change is to provide a reusable and consistent profile icon across the application.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import { ProfileIcon } from '@/assets/icons/ProfileSVG';
 
 export default function Navbar() {
     return (
         <div className="relative flex justify-between items-center min-w-[100%]">
-            <div className="border border-black min-w-[100%] z-50 h-16 border-t-0 border-l-0 border-r-0">
+            <div className="border border-black dark:border-white min-w-[100%] z-50 h-16 border-t-0 border-l-0 border-r-0">
                 <div className="absolute left-0 flex items-center pl-5 m-auto h-full ">
                     <MountainIcon />
                 </div>
@@ -12,9 +13,12 @@ export default function Navbar() {
                 </nav>
             </div>
 
-            <nav className=" hidden md:lg:flex flex-col gap-4 fixed top-16 border-t-0 border-l-0 left-0 h-[100vh] w-16 bg-[#ac965c] pt-2 border border-black ">
-                <NavLinkAside href="/login">In</NavLinkAside>
-                <NavLinkAside href="/register">En</NavLinkAside>
+            <nav className=" hidden md:lg:flex flex-col gap-4 fixed top-16 border-t-0 border-l-0 left-0 h-[100vh] w-16 bg-[#ac965c] pt-2 border border-black dark:border-white ">
+                <Link
+                    className="relative font-medium text-white text-base transition-colors hover:text-gray-300 w-full text-center"
+                    href="/">
+                    <ProfileIcon />
+                </Link>
             </nav>
         </div>
     );
